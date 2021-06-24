60f14d0515e85288a994ca6da2705f71ea07a940
rito15 <ghdtkals15@naver.com>
2021-06-25 03:19:14 +0900

    [2021-06-25] Update

--- docs/assets/js/data/cache-list.js
+++ docs/assets/js/data/cache-list.js
@@ -2,21 +2,21 @@ const include = [
 
   /*--- CSS ---*/
 
-  '/Portfolio/assets/css/home.css',
-  '/Portfolio/assets/css/categories.css',
-  '/Portfolio/assets/css/tags.css',
-  '/Portfolio/assets/css/archives.css',
-  '/Portfolio/assets/css/page.css',
-  '/Portfolio/assets/css/post.css',
-  '/Portfolio/assets/css/category-tag.css',
-  '/Portfolio/assets/css/lib/bootstrap-toc.min.css',
+  '/assets/css/home.css',
+  '/assets/css/categories.css',
+  '/assets/css/tags.css',
+  '/assets/css/archives.css',
+  '/assets/css/page.css',
+  '/assets/css/post.css',
+  '/assets/css/category-tag.css',
+  '/assets/css/lib/bootstrap-toc.min.css',
 
   /*--- Javascripts ---*/
 
-  '/Portfolio/assets/js/home.min.js',
-  '/Portfolio/assets/js/page.min.js',
-  '/Portfolio/assets/js/post.min.js',
-  '/Portfolio/assets/js/categories.min.js',
+  '/assets/js/home.min.js',
+  '/assets/js/page.min.js',
+  '/assets/js/post.min.js',
+  '/assets/js/categories.min.js',
 
   /*--- HTML ---*/
 
@@ -31,33 +31,33 @@ const include = [
     '/tabs/about/',
   
 
-  /*--- Icons ---*/'/Portfolio/assets/img/favicons/favicon.ico',
-  '/Portfolio/assets/img/favicons/apple-icon.png',
-  '/Portfolio/assets/img/favicons/apple-icon-precomposed.png',
-  '/Portfolio/assets/img/favicons/apple-icon-57x57.png',
-  '/Portfolio/assets/img/favicons/apple-icon-60x60.png',
-  '/Portfolio/assets/img/favicons/apple-icon-72x72.png',
-  '/Portfolio/assets/img/favicons/apple-icon-76x76.png',
-  '/Portfolio/assets/img/favicons/apple-icon-114x114.png',
-  '/Portfolio/assets/img/favicons/apple-icon-120x120.png',
-  '/Portfolio/assets/img/favicons/apple-icon-144x144.png',
-  '/Portfolio/assets/img/favicons/apple-icon-152x152.png',
-  '/Portfolio/assets/img/favicons/apple-icon-180x180.png',
-  '/Portfolio/assets/img/favicons/android-icon-192x192.png',
-  '/Portfolio/assets/img/favicons/favicon-32x32.png',
-  '/Portfolio/assets/img/favicons/favicon-96x96.png',
-  '/Portfolio/assets/img/favicons/favicon-16x16.png',
-  '/Portfolio/assets/img/favicons/ms-icon-144x144.png',
-  '/Portfolio/assets/img/favicons/manifest.json',
-  '/Portfolio/assets/img/favicons/browserconfig.xml',
+  /*--- Icons ---*/'/assets/img/favicons/favicon.ico',
+  '/assets/img/favicons/apple-icon.png',
+  '/assets/img/favicons/apple-icon-precomposed.png',
+  '/assets/img/favicons/apple-icon-57x57.png',
+  '/assets/img/favicons/apple-icon-60x60.png',
+  '/assets/img/favicons/apple-icon-72x72.png',
+  '/assets/img/favicons/apple-icon-76x76.png',
+  '/assets/img/favicons/apple-icon-114x114.png',
+  '/assets/img/favicons/apple-icon-120x120.png',
+  '/assets/img/favicons/apple-icon-144x144.png',
+  '/assets/img/favicons/apple-icon-152x152.png',
+  '/assets/img/favicons/apple-icon-180x180.png',
+  '/assets/img/favicons/android-icon-192x192.png',
+  '/assets/img/favicons/favicon-32x32.png',
+  '/assets/img/favicons/favicon-96x96.png',
+  '/assets/img/favicons/favicon-16x16.png',
+  '/assets/img/favicons/ms-icon-144x144.png',
+  '/assets/img/favicons/manifest.json',
+  '/assets/img/favicons/browserconfig.xml',
 
   /*--- Others ---*/
 
-  '/Portfolio/assets/js/data/search.json',
-  '/Portfolio/404.html',
+  '/assets/js/data/search.json',
+  '/404.html',
 
-  '/Portfolio/app.js',
-  '/Portfolio/sw.js'
+  '/app.js',
+  '/sw.js'
 ];
 
 const exclude = ['/assets/js/data/pageviews.json',
--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
-self.importScripts('/Portfolio/assets/js/data/cache-list.js');
+self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210625.0316';
+var cacheName = 'chirpy-20210625.0318';
 
 
 function isExcluded(url) {
