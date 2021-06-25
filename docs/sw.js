9720ac1e0e8edb1710f7eee0e949d8829dbbe920
rito15 <ghdtkals15@naver.com>
2021-06-25 16:21:40 +0900

    [2021-06-25] Update

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
-self.importScripts('/assets/js/data/cache-list.js');
+self.importScripts('/Portfolio/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210625.1620';
+var cacheName = 'chirpy-20210625.1621';
 
 
 function isExcluded(url) {
