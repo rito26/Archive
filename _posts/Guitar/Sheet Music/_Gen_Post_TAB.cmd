f4e488b439fd8af372f4592873bbe5a630d0b1f2
rito15 <rito26@naver.com>
2023-03-11 00:21:58 +0900

    [2023-03-11] Update

--- _posts/Guitar/Sheet Music/_Gen_Post_TAB.cmd
+++ _posts/Guitar/Sheet Music/_Gen_Post_TAB.cmd
@@ -49,7 +49,7 @@ echo.>> %strFileDir%
 echo.# Video>> %strFileDir%
 echo.--- >> %strFileDir%
 echo.>> %strFileDir%
-echo.^<style^>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }^</style^>^<div class='embed-container'^>^<iframe src='https://www.youtube.com/embed/____________________' frameborder='0' allowfullscreen^>^</iframe^>^</div^>>> %strFileDir%
+echo.^<style^>.embed-container { position: relative; padding-bottom: 56.25%%; height: 0; overflow: hidden; max-width: 100%%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%%; height: 100%%; }^</style^>^<div class='embed-container'^>^<iframe src='https://www.youtube.com/embed/___________' frameborder='0' allowfullscreen^>^</iframe^>^</div^>>> %strFileDir%
 echo.>> %strFileDir%
 echo.>> %strFileDir%
 echo.# Tab>> %strFileDir%
--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/Archive/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20230307.2237';
+var cacheName = 'chirpy-20230311.0021';
 
 
 function isExcluded(url) {
